dc7ba8d62055fad38bdf70b5ffb47d31c64def6b
RandomRamen <pt072093@gmail.com>
2019-11-03 15:27:11 -0600

    Updates

---
+++ precache-manifest.35d591f35fb461dcda09e6566e230c39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ed5359c1b8f1f143915f209fe77158",
+    "url": "/react-collectors/index.html"
+  },
+  {
+    "revision": "1c646a8a6a9ab9108d20",
+    "url": "/react-collectors/static/css/main.dccb7e09.chunk.css"
+  },
+  {
+    "revision": "793b0eb69e9b6fdfc64f",
+    "url": "/react-collectors/static/js/2.4aef4bc4.chunk.js"
+  },
+  {
+    "revision": "1c646a8a6a9ab9108d20",
+    "url": "/react-collectors/static/js/main.8d12821e.chunk.js"
+  },
+  {
+    "revision": "65be955dfbd1bc902147",
+    "url": "/react-collectors/static/js/runtime-main.ab8025c3.js"
+  },
+  {
+    "revision": "20e6a8c6e1d7d40a99f45379788a6ddc",
+    "url": "/react-collectors/static/media/pokedex.20e6a8c6.png"
+  },
+  {
+    "revision": "3e7e24822cfab829d8641f084620da5e",
+    "url": "/react-collectors/static/media/unknown-h.3e7e2482.jpg"
+  }
+]);
